2c8bde17c36b457fd19b00b6aacc984fcd2d535a
Sebastien Klasa <klasa.sebastien@gmail.com>
2018-09-18 12:13:30 +0200

    Add CMake project

---
+++ CrackDetection/CrackDetection/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+
+	Mat imageIn = imread(argv[1], IMREAD_GRAYSCALE);
+	if (!imageIn.data)
+	{
+		cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+	imshow("Display window", imageIn);
+	waitKey(0);
+	return 0;
+}

bb66434e61232513e3a5d34e84f786a37144e8d1
Sebastien Klasa <klasa.sebastien@gmail.com>
2018-10-09 19:24:54 +0000

    Add benchmark.sh

---
+++ benchmark.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -lt 3 ]; then
+    echo "usage: $0 <algorithm> <precision> <recall> <images_folder>"
+fi
+
+algorithm=$1
+precision=$2
+recall=$3
+folder=$4
+
+inputs=("6192.jpg" "6193.jpg" "6194.jpg" "6195.jpg" "v001.ppm" "v002.ppm" "v003.ppm" "v004.ppm")
+reals=("6192.bmp" "6193.bmp" "6194.bmp" "6195.bmp" "t001.pgm" "t002.pgm" "t003.pgm" "t004.pgm")
+
+rm scores.csv
+
+for (( i=0; i<${#inputs[@]}; i++ )); do
+    input=${inputs[$i]}
+    real=${reals[$i]}
+    ./$algorithm $folder/$input output.png
+    p=`./$precision $folder/$real output.png`
+    r=`./$recall $folder/$real output.png`
+    echo "$input: $p $r"
+    echo "$p, $r" >> scores.csv
+done
---
+++ src/precision.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#include "scoring.hpp"
+#include "arguments.hpp"
+
+int main(int argc, char** argv) {
+
+	arguments_t args(argc, argv);
+
+	if (args.parametersSize() < 2) {
+		std::cout << "usage: " << argv[0] << " truth binary_image" << std::endl;
+		return 0;
+	}
+
+	cv::Mat matTruth = cv::imread(args[0], cv::IMREAD_GRAYSCALE);
+	if (!matTruth.data) {
+		std::cout << "Cannot load " << args[0] << std::endl;
+		return 0;
+	}
+
+	cv::Mat matOutput = cv::imread(args[1], cv::IMREAD_GRAYSCALE);
+	if (!matOutput.data) {
+		std::cout << "Cannot load " << args[1] << std::endl;
+		return 0;
+	}
+
+	double precision, recall;
+	score(matTruth, matOutput, 1, precision, recall);
+
+	std::cout << precision << std::endl;
+
+	return 0;
+}

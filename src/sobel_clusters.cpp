e4eb62ca9be06b94862d1bf258fd09bd6252453e
Sebastien Klasa <klasa.sebastien@gmail.com>
2018-10-10 11:37:15 +0200

    Add sobel_cluster

---
+++ src/sobel_clusters.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#include "arguments.hpp"
+#include "filters.hpp"
+
+int main(int argc, char** argv) {
+    arguments_t args(argc, argv);
+
+	if (args.parametersSize() < 2) {
+		std::cout << "usage: " << argv[0] << " input output" << std::endl;
+		return 0;
+	}
+
+    auto image = cv::imread(args[0], cv::IMREAD_GRAYSCALE);
+	if (!image.data) {
+		std::cout << "Cannot load " << args[0] << std::endl;
+		return 0;
+	}
+
+	gaussianFilter(image, 5, 1.5);
+    sobelFilter(image);
+    binaryFilter(image, 0.2);
+    clusterRemoval(image, 200);
+
+    cv::imshow("result", image);
+    cv::waitKey();
+
+    cv::imwrite(args[1], image);
+
+    return 0;
+}
